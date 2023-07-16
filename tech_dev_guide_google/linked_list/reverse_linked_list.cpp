8e194adb301fb1683af6033456b1d42b5ab003b5
Tusgavin <dias.gdo@gmail.com>
2023-07-16 14:11:04 -0300

    reverse linked list

---
+++ tech_dev_guide_google/linked_list/reverse_linked_list.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        
+        ListNode* front = head;
+        ListNode* back = head;
+        
+        while (front->next != nullptr) {
+            front = front->next;
+            back->next = front->next;
+            front->next = head;
+            head = front;
+            front = back;
+        }
+        
+        return head;
+    }
+};
